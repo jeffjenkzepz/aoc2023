a8c6d1ca3b6aaef636c8cb47ec1114db3e169221
Jeff Jenkinson <jjenkinson@worldremit.com>
2023-12-09 08:35:23 +0200

    Adding local caching for inputs

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,8 +1,23 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
 export const fetchInput = async (path: string): Promise<string> => {
+  const fileName = path
+    .replace('https://adventofcode.com/', '')
+    .split('/')
+    .join('-');
+
+  // aoc input doesnt change once you have it so
+  // if we have a cached result fetch that (respect their server)
+  // if you wanna bust cache for any reason just delete the file in inputs/
+  if (fs.existsSync(`./inputs/${fileName}`)) {
+    console.log('Cache hit returning cached input', fileName);
+    const data = fs.readFileSync(`./inputs/${fileName}`, 'utf8');
+    return data.trim();
+  }
+
   const response = await fetch(path, {
     headers: {
       Cookie: process.env.COOKIE!
@@ -10,6 +25,9 @@ export const fetchInput = async (path: string): Promise<string> => {
   });
 
   const data = await response.text();
+
+  // cache the input
+  fs.writeFileSync(`./inputs/${fileName}`, data);
 
   return data.trim();
 };
